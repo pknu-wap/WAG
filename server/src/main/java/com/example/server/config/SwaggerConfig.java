95b9f7b37172ae338f278600dc720df65818611e
JongKyeong <111286262+JONG-KYEONG@users.noreply.github.com>
2024-03-24 13:38:19 +0900

    Add files via upload

---
+++ server/src/main/java/com/example/server/config/SwaggerConfig.java
@@ -0,0 +1,45 @@
+package com.example.server.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+
+        Info info = new Info()
+                .version("v1.0.0")
+                .title("API 타이틀")
+                .description("API Description");
+
+        String key = "Access Token (Bearer)";
+        String refreshKey = "Refresh Token";
+
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList(key)
+                .addList(refreshKey);
+
+        SecurityScheme accessTokenSecurityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("Bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name(HttpHeaders.AUTHORIZATION);
+
+        Components components = new Components()
+                .addSecuritySchemes(key, accessTokenSecurityScheme);
+
+        return new OpenAPI()
+                .info(info)
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+}
