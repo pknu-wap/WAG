edbd4d3e30b455ba216d1bf2fe8b70ebcbb3183c
JongKyeong Kim <oo7bangjk@naver.com>
2024-03-27 14:23:57 +0900

    소켓 & dto 추가

--- server/src/main/java/com/example/server/config/WebSocketConfig.java
+++ server/src/main/java/com/example/server/config/WebSocketConfig.java
@@ -11,14 +11,13 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/stomp/chat")  //웹소켓의 엔드포인트를 추가합니다. witSockJS()는 웹소켓을 지원하지 않는 브라우저에서도 작동하게 합니다.
+        registry.addEndpoint("/ws")  //웹소켓의 엔드포인트를 추가합니다. witSockJS()는 웹소켓을 지원하지 않는 브라우저에서도 작동하게 합니다.
                 .setAllowedOrigins("http://localhost:8080")
                 .withSockJS();
-
     }
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/app");     // 주제 앞에 /app경로를 고정합니다.
-        registry.enableSimpleBroker("/topic");   // topic이라는 주제를 생성
+        registry.setApplicationDestinationPrefixes("/pub");     // 이 주소로 메시지 전송한다.
+        registry.enableSimpleBroker("/sub");   // 이 주소로 매시지 전달 받는다.
     }
 }
--- server/src/main/java/com/example/server/controller/WebSocketEventListener.java
+++ server/src/main/java/com/example/server/controller/WebSocketEventListener.java
@@ -2,6 +2,7 @@ package com.example.server.controller;
 
 import com.example.server.domain.ChatMessage;
 import com.example.server.repository.ChatRoomRepository;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
@@ -10,7 +11,6 @@ import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionConnectedEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
-import org.slf4j.Logger;
 
 @Component
 public class WebSocketEventListener {
