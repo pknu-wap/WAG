798091969a1adc9e3957e026d65557a319018959
JS <80705329+dev-junseo@users.noreply.github.com>
2024-03-27 16:57:17 +0900

    #17 다크모드 생성

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
+import ThemeButton from "./component/Theme/themeButton";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-sm">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Reactttttttt
-          Learn React
-        </a>
-      </header>
+      <ThemeButton></ThemeButton>
     </div>
   );
 }
