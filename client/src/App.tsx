e2bcf02818676bbe941fd9f8c05970740961dc33
JS <80705329+dev-junseo@users.noreply.github.com>
2024-03-29 09:15:14 +0900

    #24 코드 수정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,25 @@
-import { useState } from 'react';
-import './App.css';
-import Button from './components/button/Button';
-import Modal from './components/modal/Modal';
+import { useState } from "react";
+import "./App.css";
+import Button from "./components/button/Button";
+import Modal from "./components/modal/Modal";
 
 function App() {
-
   const [openModal, setOpenModal] = useState(false);
   const modalOnClick = () => {
-    setOpenModal(!openModal)
-  }
+    setOpenModal(!openModal);
+  };
 
   return (
     <div className="App">
-      <div className="justify-center text-9xl mb-20 text-black dark:text-dark-text">WAG</div>
+      <div className="justify-center text-9xl mb-20">WAG</div>
       <div className="flex flex-col items-center justify-center space-y-5">
-        <Button size="lg" >방 생성</Button>
-        <Button size="lg" >방 참가</Button>
-        <Button size="lg" >랜덤 참가</Button>
-        <Button size="md" >순위 보러가기</Button>
-        <Button onClick={modalOnClick} size="md" >모달 열어보기</Button>
+        <Button size="lg">방 생성</Button>
+        <Button size="lg">방 참가</Button>
+        <Button size="lg">랜덤 참가</Button>
+        <Button size="md">순위 보러가기</Button>
+        <Button onClick={modalOnClick} size="md">
+          모달 열어보기
+        </Button>
       </div>
       {openModal && <Modal onOpenModal={modalOnClick} />}
     </div>
