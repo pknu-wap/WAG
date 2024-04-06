4512aefe63fc42e97d64f918b4684d9f8fae4e91
JS <80705329+dev-junseo@users.noreply.github.com>
2024-04-06 17:50:41 +0900

    #48 게임 대기방 UI 제작

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,15 @@
 import { Route, Routes } from "react-router-dom";
-import GamePlaying from "./GamePlaying/GamePlaying";
+import GamePlaying from "./Game/GamePlaying";
 import MainPage from "./main/MainPage";
 import FullLayout from "./components/layout/FullLayout";
+import ReadyToGame from "./Game/ReadyToGame";
 
 function App() {
   return (
     <FullLayout>
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="/ReadyToGame" element={<ReadyToGame />} />
         <Route path="/GamePlaying" element={<GamePlaying />} />
       </Routes>
     </FullLayout>
