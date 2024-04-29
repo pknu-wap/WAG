8631545bed5d080c911a3adb1df9e6dcc92b67e3
Junseo Kim <semiwest2@gmail.com>
2024-04-29 22:32:16 +0900

    페이지 라우팅 구현

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,17 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import GamePlaying from "./Game/GamePlaying";
 import MainPage from "./main/MainPage";
 import FullLayout from "./components/layout/FullLayout";
 import ReadyToGame from "./Game/ReadyToGame";
 import JoinGame from "./Game/JoinGame";
+import CreateRoom from "./CreateRoom/CreateRoom";
 
 function App() {
   return (
     <FullLayout>
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="/CreateRoom" element={<CreateRoom />} />
         <Route path="/JoinGame" element={<JoinGame />} />
         <Route path="/ReadyToGame" element={<ReadyToGame />} />
         <Route path="/GamePlaying" element={<GamePlaying />} />
