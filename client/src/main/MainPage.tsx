6122828877adc319dc2a105d692b0e77c143e58a
Junseo Kim <semiwest2@gmail.com>
2024-04-30 15:05:35 +0900

    Update MainPage.tsx

--- client/src/main/MainPage.tsx
+++ client/src/main/MainPage.tsx
@@ -44,6 +44,7 @@ function MainPage({ dark }: ComponentProps) {
   //   setRoomIdCode(enterCodeData)
   // }, [enterCodeData])
 
+  //입장코드로 입력으로 roomid받기
   const getRoomIdCode = async (): Promise<IGetRoomIdCode> => {
     try {
       const response = await axios.get<IGetRoomIdCode>(
@@ -60,9 +61,42 @@ function MainPage({ dark }: ComponentProps) {
       throw e;
     }
   };
+
+  //빠른 입장으로 roomid받기
+  const getRandomRoomId = async () => {
+    try {
+      const response = await axios.get("http://wwwag.co.kr:8080/roomId");
+      return response.data; // 서버로부터 받은 데이터 반환
+    } catch (error) {
+      console.error("랜덤 입장 요청 중 오류 발생:", error);
+      throw error;
+    }
+  };
+//빠른 입장 버튼 클릭 이벤트 핸들러
+const handleRandomEnterClick = async () => {
+  try {
+    const data = await getRandomRoomId(); // API 호출
+    if (data.roomId) {
+      // roomId가 존재하는 경우, 해당 방 ID로 소켓 연결
+      console.log("입장할 방 ID:", data.roomId);
+      //const socket = io(`http://wwwag.co.kr:8080/topic/public/${data.roomId}`); // 해당 방으로 소켓 연결
+      const socket = io(`http://wwwag.co.kr:8080/topic/public/2`); // 해당 방으로 소켓 연결
+      navigate('/JoinGame');
+      // 소켓 이벤트 리스너 등록 및 기타 로직 처리
+      // 예시: navigate(`/room/${data.roomId}`); // 방 페이지로 이동
+    } else if (data === "no available room") {
+      // 사용 가능한 방이 없는 경우
+      alert("사용 가능한 방이 없습니다.");
+    }
+  } catch (error) {
+    console.error("랜덤 입장 처리 중 오류 발생:", error);
+  }
+};
+
+
   const buttonCheckHandler = () => {
     const roomIdFromCode = getRoomIdCode();
-    const socket = io(`http://wwwag.co.kr:8080/topic/public/${roomIdFromCode}`); //해당 방으로 소켓 연결
+    const socket = io(`http://wwwag.co.kr:8080/topic/public/`); //해당 방으로 소켓 연결
     console.log(roomIdFromCode);
   };
   const navigate = useNavigate(); // useNavigate 훅을 컴포넌트 내부에서 사용
@@ -97,11 +131,11 @@ function MainPage({ dark }: ComponentProps) {
         <img src="images/WAG_dark.2.png" alt="logo dark mode"></img>
       )}
       <div className="flex flex-col items-center justify-center space-y-5">
-        <Button size="lg" onClick={openModal}>
-          랜덤 참가
+        <Button size="lg" onClick={handleRandomEnterClick}>
+          랜덤 입장
         </Button>
         <Button size="lg" onClick={handleCreateRoomClick}>방 생성</Button>
-        <Button size="lg">방 참가</Button>
+        <Button size="lg" onClick={openModal}>입장코드 입력</Button>
       </div>
       <Modal onRequestClose={closeModal}>
         <div className="flex flex-col justify-between">
