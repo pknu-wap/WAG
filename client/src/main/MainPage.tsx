b6b743686bf62777969e9160d6668f7d0b7261cb
JS <80705329+dev-junseo@users.noreply.github.com>
2024-04-30 19:49:11 +0900

    #80 소켓 연결 성공?

--- client/src/main/MainPage.tsx
+++ client/src/main/MainPage.tsx
@@ -11,8 +11,9 @@ import { faX } from "@fortawesome/free-solid-svg-icons";
 import axios from "axios";
 import { IGetRoomIdCode } from "../types/dto";
 import { io } from "socket.io-client";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import * as StompJs from "@stomp/stompjs";
+import SockJS from "sockjs-client";
 
 type Props = {
   children?: React.ReactNode;
@@ -26,6 +27,10 @@ const connector = connect(
 );
 
 function MainPage({ dark }: ComponentProps) {
+  const socketUrl = "http://wwwag.co.kr:8080";
+  const sock = new SockJS(socketUrl);
+  const ws = StompJs.Stomp.over(sock);
+
   const [theme, setTheme] = useState(localStorage.theme);
   const [enterCode, setEnterCode] = useState<number>();
   const [, setIsOpen] = useRecoilState(modalState);
@@ -74,43 +79,40 @@ function MainPage({ dark }: ComponentProps) {
     }
   };
 
+  //빠른 입장 버튼 클릭 이벤트 핸들러
+  const handleRandomEnterClick = async () => {
+    try {
+      const roomId = await getRandomRoomId(); // API 호출
+      if (roomId) {
+        // roomId가 존재하는 경우, 해당 방 ID로 소켓 연결
+        console.log("입장할 방 ID:", roomId);
 
-//빠른 입장 버튼 클릭 이벤트 핸들러 
-const handleRandomEnterClick = async () => {
-  try {
-    const roomId = await getRandomRoomId(); // API 호출
-    if (roomId) {
-      // roomId가 존재하는 경우, 해당 방 ID로 소켓 연결
-      console.log("입장할 방 ID:", roomId);
+        ws.connect({}, (roomId: number) => {
+          ws.subscribe(`/topic/public/${roomId}`, (data) => {});
+        });
 
-      
-
-
-      navigate('/JoinGame');
-      // 소켓 이벤트 리스너 등록 및 기타 로직 처리
-      // 예시: navigate(`/room/${data.roomId}`); // 방 페이지로 이동
-    } else if (roomId === "no available room") {
-      // 사용 가능한 방이 없는 경우
-      alert("사용 가능한 방이 없습니다.");
+        navigate("/JoinGame");
+        // 소켓 이벤트 리스너 등록 및 기타 로직 처리
+        // 예시: navigate(`/room/${data.roomId}`); // 방 페이지로 이동
+      } else if (roomId === "no available room") {
+        // 사용 가능한 방이 없는 경우
+        alert("사용 가능한 방이 없습니다.");
+      }
+    } catch (error) {
+      console.error("랜덤 입장 처리 중 오류 발생:", error);
     }
-  } catch (error) {
-    console.error("랜덤 입장 처리 중 오류 발생:", error);
-  }
-};
-
+  };
 
   const buttonCheckHandler = () => {
     const roomIdFromCode = getRoomIdCode();
-    const socket = io(`http://wwwag.co.kr:8080/topic/public/`); //해당 방으로 소켓 연결
+    // const socket = io(`http://wwwag.co.kr:8080/topic/public/`); //해당 방으로 소켓 연결
     console.log(roomIdFromCode);
   };
   const navigate = useNavigate(); // useNavigate 훅을 컴포넌트 내부에서 사용
 
   const handleCreateRoomClick = () => {
-    navigate('/CreateRoom'); // 페이지 이동 처리
+    navigate("/CreateRoom"); // 페이지 이동 처리
   };
-
-
 
   useEffect(() => {
     if (enterCode === undefined || Number.isNaN(enterCode)) {
@@ -139,8 +141,12 @@ const handleRandomEnterClick = async () => {
         <Button size="lg" onClick={handleRandomEnterClick}>
           랜덤 입장
         </Button>
-        <Button size="lg" onClick={handleCreateRoomClick}>방 생성</Button>
-        <Button size="lg" onClick={openModal}>입장코드 입력</Button>
+        <Button size="lg" onClick={handleCreateRoomClick}>
+          방 생성
+        </Button>
+        <Button size="lg" onClick={openModal}>
+          입장코드 입력
+        </Button>
       </div>
       <Modal onRequestClose={closeModal}>
         <div className="flex flex-col justify-between">
