0e062c58100fc8ec8770198467f00fcf46652bab
Junseo Kim <semiwest2@gmail.com>
2024-04-30 19:03:47 +0900

    Update MainPage.tsx

--- client/src/main/MainPage.tsx
+++ client/src/main/MainPage.tsx
@@ -72,19 +72,19 @@ function MainPage({ dark }: ComponentProps) {
       throw error;
     }
   };
-//빠른 입장 버튼 클릭 이벤트 핸들러
+//빠른 입장 버튼 클릭 이벤트 핸들러 
 const handleRandomEnterClick = async () => {
   try {
-    const data = await getRandomRoomId(); // API 호출
-    if (data.roomId) {
+    const roomId = await getRandomRoomId(); // API 호출
+    if (roomId) {
       // roomId가 존재하는 경우, 해당 방 ID로 소켓 연결
-      console.log("입장할 방 ID:", data.roomId);
+      console.log("입장할 방 ID:", roomId);
       //const socket = io(`http://wwwag.co.kr:8080/topic/public/${data.roomId}`); // 해당 방으로 소켓 연결
       const socket = io(`http://wwwag.co.kr:8080/topic/public/2`); // 해당 방으로 소켓 연결
       navigate('/JoinGame');
       // 소켓 이벤트 리스너 등록 및 기타 로직 처리
       // 예시: navigate(`/room/${data.roomId}`); // 방 페이지로 이동
-    } else if (data === "no available room") {
+    } else if (roomId === "no available room") {
       // 사용 가능한 방이 없는 경우
       alert("사용 가능한 방이 없습니다.");
     }
