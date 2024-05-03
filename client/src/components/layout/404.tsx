a795eacfda63e9f1c7f85a48ad2d4bc2c7faf13b
JS <80705329+dev-junseo@users.noreply.github.com>
2024-05-03 19:51:54 +0900

    #135 404 컴포넌트 생성

---
+++ client/src/components/layout/404.tsx
@@ -0,0 +1,11 @@
+import FullLayout from "./FullLayout";
+
+function WrongUrl() {
+  return (
+    <FullLayout>
+      <div className="text-9xl">404</div>
+    </FullLayout>
+  );
+}
+
+export default WrongUrl;
