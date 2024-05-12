c5bc83d7a8c4178b65b931ec3b2fead6ab85f69c
“PororoAndFriends” <gbham0815@pukyong.ac.kr>
2024-05-12 19:52:56 +0900

    update game record

--- server/src/main/java/com/example/server/controller/RoomController.java
+++ server/src/main/java/com/example/server/controller/RoomController.java
@@ -7,7 +7,6 @@ import com.example.server.security.CurrentUser;
 import com.example.server.security.UserPrincipal;
 import com.example.server.service.RoomService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
