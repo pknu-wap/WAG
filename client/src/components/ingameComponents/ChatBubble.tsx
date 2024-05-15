410f05ca3f72b56f90c04cfce75bde0d7478297c
JS <80705329+dev-junseo@users.noreply.github.com>
2024-05-15 19:25:30 +0900

    #160 UI 개선

--- client/src/components/ingameComponents/ChatBubble.tsx
+++ client/src/components/ingameComponents/ChatBubble.tsx
@@ -2,46 +2,62 @@ import React, { useState, useEffect } from "react";
 import { ChatMessage } from "../../types/dto";
 
 const ChatBubble: React.FC<{ message: ChatMessage }> = ({ message }) => {
-    const [chatMessages, setChatMessages] = useState<ChatMessage[]>([]);
-    const myName = localStorage.getItem('nickName');
+  const [chatMessages, setChatMessages] = useState<ChatMessage[]>([]);
+  const myName = localStorage.getItem("nickName");
 
-    // 새 메시지가 도착할 때마다 채팅창 스크롤을 아래로 이동합니다.
-    useEffect(() => {
-        const chatContainer = document.getElementById('chat-container');
-        if (chatContainer) {
-            chatContainer.scrollTop = chatContainer.scrollHeight;
-        }
-    }, [chatMessages]);
+  // 새 메시지가 도착할 때마다 채팅창 스크롤을 아래로 이동합니다.
+  useEffect(() => {
+    const chatContainer = document.getElementById("chat-container");
+    if (chatContainer) {
+      chatContainer.scrollTop = chatContainer.scrollHeight;
+    }
+  }, [chatMessages]);
 
-    useEffect(() => {
-        // 이전 메시지 배열에 새 메시지를 추가합니다.
-        setChatMessages(prevMessages => [...prevMessages, message]);
-    }, [message]);
+  useEffect(() => {
+    // 이전 메시지 배열에 새 메시지를 추가합니다.
+    setChatMessages((prevMessages) => [...prevMessages, message]);
+  }, [message]);
 
-    return (
-        <div id="chat-container" className="mt-1 overflow-auto h-64 flex flex-col">
-            {chatMessages.map((msg, index) => (
-                <div key={index} className={`flex flex-col items-${msg.sender === myName ? 'end' : msg.messageType === 'JOIN' || msg.messageType === 'LEAVE' ? 'middle' : 'start'}`}>
-                     {msg.messageType === 'JOIN' || msg.messageType === 'LEAVE' ? (
-                        <>
-                            {msg.sender !== myName ? (
-                                <>
-                                <span className="text-white">{msg.content}</span>
-                                </>
-                            ) :(<></>)}
-                        </>
-                    ) : (
-                        <>
-                            <span className="text-white">{msg.sender}</span>
-                            <span className={`w-auto h-auto px-4 rounded-lg rounded-tl-none bg-light-chat dark:bg-dark-btn`}>
-                                {msg.content}
-                            </span>
-                        </>
-                    )}
-                </div>
-            ))}
+  return (
+    <div
+      id="chat-container"
+      className="mt-1 overflow-auto h-full flex flex-col p-5"
+    >
+      {chatMessages.map((msg, index) => (
+        <div
+          key={index}
+          className={`flex flex-col items-${
+            msg.sender === myName
+              ? "end"
+              : msg.messageType === "JOIN" || msg.messageType === "LEAVE"
+              ? "middle"
+              : "start"
+          }`}
+        >
+          {msg.messageType === "JOIN" || msg.messageType === "LEAVE" ? (
+            <>
+              {msg.sender !== myName ? (
+                <>
+                  <span className="text-white">{msg.content}</span>
+                </>
+              ) : (
+                <></>
+              )}
+            </>
+          ) : (
+            <>
+              <span className="text-white">{msg.sender}</span>
+              <span
+                className={`w-auto h-auto px-4 rounded-lg rounded-tr-none bg-light-chat dark:bg-dark-btn`}
+              >
+                {msg.content}
+              </span>
+            </>
+          )}
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default ChatBubble;
