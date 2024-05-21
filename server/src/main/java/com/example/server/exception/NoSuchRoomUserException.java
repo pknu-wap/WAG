9145492cd813a0a5e17c6421c0218cb034c487b9
“PororoAndFriends” <gbham0815@pukyong.ac.kr>
2024-05-21 15:53:11 +0900

    add exception and fix room exit error

---
+++ server/src/main/java/com/example/server/exception/NoSuchRoomUserException.java
@@ -0,0 +1,12 @@
+package com.example.server.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class NoSuchRoomUserException extends RuntimeException{
+
+    public NoSuchRoomUserException() {
+        super("no such room user");
+    }
+}
