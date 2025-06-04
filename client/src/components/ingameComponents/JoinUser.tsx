edb4966601099e1bd265663c7e7a52cf69c12970
Junseo Kim <semiwest2@gmail.com>
2025-06-04 20:02:57 +0900

    refactor: API URL 분리
    - API 요청 시 환경변수(REACT_APP_API_URL) 사용으로 URL 하드코딩 제거

--- client/src/components/ingameComponents/JoinUser.tsx
+++ client/src/components/ingameComponents/JoinUser.tsx
@@ -29,23 +29,23 @@ const JoinUser = forwardRef<HTMLDivElement, JoinUserProps>(
     const [penaltyCount, setPenaltyCount] = useState(0); // 페널티 갯수 체크
     const [ready, setReady] = useState(false) // 레디 체크
 
-    const [width, setWidth] = useState<number>(window.innerWidth); 
-    
+    const [width, setWidth] = useState<number>(window.innerWidth);
+
     const handleResize = () => {
       setWidth(window.innerWidth);
-     };
-  
+    };
+
     useEffect(() => {
       window.addEventListener("resize", handleResize);
       return () => {
         window.removeEventListener("resize", handleResize);
       }
-    },[])
-    
+    }, [])
+
     const getGameAnswer = async () => {
       try {
         const response = await axios.get<IGetAnswerList>(
-          "https://wwwag-backend.co.kr:5000/answer/list",
+          `${process.env.REACT_APP_API_URL}/answer/list`,
           {
             params: {
               roomId: roomId,
@@ -108,14 +108,14 @@ const JoinUser = forwardRef<HTMLDivElement, JoinUserProps>(
 
     // 순위 확인
     const checkToRank = () => {
-     gameUserDto.forEach((dto) => {
-      if (Nickname === dto.roomNickname) {
-        setUserRank(dto.ranking)
-        if (1<= dto.ranking && dto.ranking <=3){
-          setPenaltyCount(0)
+      gameUserDto.forEach((dto) => {
+        if (Nickname === dto.roomNickname) {
+          setUserRank(dto.ranking)
+          if (1 <= dto.ranking && dto.ranking <= 3) {
+            setPenaltyCount(0)
+          }
         }
-      }
-     }) 
+      })
     }
 
     // 누구 턴인지 확인
@@ -130,36 +130,36 @@ const JoinUser = forwardRef<HTMLDivElement, JoinUserProps>(
         className={`${className} flex flex-col justify-center items-center relative mx-1`}
       >
         {Nickname === whoseTurn ? ( // 내 턴일 때 주황색으로 프로필 색깔 바뀌도록
-        <PopoverComponent 
-          isOpen={isPopoverOpen} onToggle={handleTogglePopover}
-          tooltipChildren={
-            children
-          }>
-          <IconButton
-          size="lg"
-          className="items-center bg-[#FFA500] dark:bg-[#FFA500] relative"
-          onClick={openTollTip}
-          disabled={gameStart && myName !== Nickname && userRank === 0 ? false : true}
-          >
-            {gameStart ? (<>
-              {width > 650 ? (
-                <div className="w-20 h-6 rounded-md sm:text-lg text-[13px] text-[#FFA500] bg-[none] flex justify-center items-center sm:bottom-16 bottom-14 absolute">
-                {answer}
-              </div>
-              ) : (
-                <div className="w-20 h-6 rounded-md sm:text-lg text-[13px] text-[#FFA500] bg-[none] flex justify-center items-center sm:bottom-15 bottom-11 absolute">
-                {answer}
-              </div>
-              )}
+          <PopoverComponent
+            isOpen={isPopoverOpen} onToggle={handleTogglePopover}
+            tooltipChildren={
+              children
+            }>
+            <IconButton
+              size="lg"
+              className="items-center bg-[#FFA500] dark:bg-[#FFA500] relative"
+              onClick={openTollTip}
+              disabled={gameStart && myName !== Nickname && userRank === 0 ? false : true}
+            >
+              {gameStart ? (<>
+                {width > 650 ? (
+                  <div className="w-20 h-6 rounded-md sm:text-lg text-[13px] text-[#FFA500] bg-[none] flex justify-center items-center sm:bottom-16 bottom-14 absolute">
+                    {answer}
+                  </div>
+                ) : (
+                  <div className="w-20 h-6 rounded-md sm:text-lg text-[13px] text-[#FFA500] bg-[none] flex justify-center items-center sm:bottom-15 bottom-11 absolute">
+                    {answer}
+                  </div>
+                )}
               </>
-            ) : !gameStart && isCaptain ? ( // 대기방에서 방장 왕관
-              <FontAwesomeIcon className="text-[#FFFF00] sm:bottom-14 bottom-10 absolute" icon={faCrown} />
-            ) : (
-              <div></div>
-            )}
-          <FontAwesomeIcon icon={faUser} size="xl" />
-          </IconButton>
-      </PopoverComponent>
+              ) : !gameStart && isCaptain ? ( // 대기방에서 방장 왕관
+                <FontAwesomeIcon className="text-[#FFFF00] sm:bottom-14 bottom-10 absolute" icon={faCrown} />
+              ) : (
+                <div></div>
+              )}
+              <FontAwesomeIcon icon={faUser} size="xl" />
+            </IconButton>
+          </PopoverComponent>
         ) : (
           <PopoverComponent
             isOpen={isPopoverOpen} onToggle={handleTogglePopover}
@@ -167,25 +167,25 @@ const JoinUser = forwardRef<HTMLDivElement, JoinUserProps>(
               children
             }>
             <IconButton
-            size="lg"
-            className={`${gameStart || isCaptain || ready ? 
-              "items-center bg-light-btn dark:bg-dark-btn relative" : 
-              "items-center bg-[#A9A9A9] dark:bg-[#A9A9A9] relative"
-            }`}
-            onClick={openTollTip}
-            disabled={gameStart && myName !== Nickname && userRank === 0 ? false : true}
+              size="lg"
+              className={`${gameStart || isCaptain || ready ?
+                "items-center bg-light-btn dark:bg-dark-btn relative" :
+                "items-center bg-[#A9A9A9] dark:bg-[#A9A9A9] relative"
+                }`}
+              onClick={openTollTip}
+              disabled={gameStart && myName !== Nickname && userRank === 0 ? false : true}
             >
               {gameStart ? (
                 <>
-                {width > 650 ? (
-                  <div className="w-20 h-6 rounded-md sm:text-lg text-[13px] text-[#FFA500] bg-[none] flex justify-center items-center sm:bottom-16 bottom-14 absolute z-50 ">
-                  {answer}
-                </div>
-                ) : (
-                  <div className="w-20 h-6 rounded-md sm:text-lg text-[13px] text-[#FFA500] bg-[none] flex justify-center items-center sm:bottom-15 bottom-11 absolute z-50">
-                  {answer}
-                </div>
-                )}
+                  {width > 650 ? (
+                    <div className="w-20 h-6 rounded-md sm:text-lg text-[13px] text-[#FFA500] bg-[none] flex justify-center items-center sm:bottom-16 bottom-14 absolute z-50 ">
+                      {answer}
+                    </div>
+                  ) : (
+                    <div className="w-20 h-6 rounded-md sm:text-lg text-[13px] text-[#FFA500] bg-[none] flex justify-center items-center sm:bottom-15 bottom-11 absolute z-50">
+                      {answer}
+                    </div>
+                  )}
                 </>
               ) : !gameStart && isCaptain ? ( // 대기방에서 방장 왕관
                 <FontAwesomeIcon className="text-[#FFFF00] sm:bottom-14 bottom-10 absolute z-50" icon={faCrown} />
@@ -201,76 +201,76 @@ const JoinUser = forwardRef<HTMLDivElement, JoinUserProps>(
           <div></div>
         ) : penaltyCount === 1 ? (
           <>
-          {width > 650 ? (
-            <div className="w-3 h-5 rounded absolute sm:top-12 sm:left-12 top-10 left-10 border-[0.1px] border-[#454141] bg-[#FFFF00]"></div>
-          ) : (
-            <div className="w-3 h-5 rounded absolute sm:top-12 sm:right-2 top-10 right-10 border-[0.1px] border-[#454141] bg-[#FFFF00]"></div>
-          )}
+            {width > 650 ? (
+              <div className="w-3 h-5 rounded absolute sm:top-12 sm:left-12 top-10 left-10 border-[0.1px] border-[#454141] bg-[#FFFF00]"></div>
+            ) : (
+              <div className="w-3 h-5 rounded absolute sm:top-12 sm:right-2 top-10 right-10 border-[0.1px] border-[#454141] bg-[#FFFF00]"></div>
+            )}
           </>
-          
+
         ) : penaltyCount === 2 ? (
           <>
-          {width > 650 ? (
-            <div className="absolute sm:top-12 sm:left-12 top-10 left-10 flex flex-column">
-            <div className="w-3 h-5 rounded border-[0.1px] border-[#454141] rotate-[160deg] bg-[#FFFF00] relative"></div>
-            <div className="w-3 h-5 rounded border-[0.1px] border-[#454141] rotate-[20deg] bg-[#FFFF00] left-[8px] top-[1px] absolute"></div>
-          </div>
-          ) : (
-            <div className="absolute sm:top-12 sm:right-2 top-10 right-10 flex flex-column">
-            <div className="w-3 h-5 rounded border-[0.1px] border-[#454141] rotate-[160deg] bg-[#FFFF00] relative"></div>
-            <div className="w-3 h-5 rounded border-[0.1px] border-[#454141] rotate-[20deg] bg-[#FFFF00] left-[8px] top-[1px] absolute"></div>
-          </div>
-          )}
-          
+            {width > 650 ? (
+              <div className="absolute sm:top-12 sm:left-12 top-10 left-10 flex flex-column">
+                <div className="w-3 h-5 rounded border-[0.1px] border-[#454141] rotate-[160deg] bg-[#FFFF00] relative"></div>
+                <div className="w-3 h-5 rounded border-[0.1px] border-[#454141] rotate-[20deg] bg-[#FFFF00] left-[8px] top-[1px] absolute"></div>
+              </div>
+            ) : (
+              <div className="absolute sm:top-12 sm:right-2 top-10 right-10 flex flex-column">
+                <div className="w-3 h-5 rounded border-[0.1px] border-[#454141] rotate-[160deg] bg-[#FFFF00] relative"></div>
+                <div className="w-3 h-5 rounded border-[0.1px] border-[#454141] rotate-[20deg] bg-[#FFFF00] left-[8px] top-[1px] absolute"></div>
+              </div>
+            )}
+
           </>
         ) : (
           <>
-          {width > 650 ? (
-            <div className="w-3 h-5 rounded absolute sm:top-12 sm:left-12 top-10 left-10 border-[0.1px] border-[#454141] bg-[#FF0000]"></div>
-          ) : (
-            <div className="w-3 h-5 rounded absolute sm:top-12 sm:right-2 top-10 right-10 border-[0.1px] border-[#454141] bg-[#FF0000]"></div>
-          )}
+            {width > 650 ? (
+              <div className="w-3 h-5 rounded absolute sm:top-12 sm:left-12 top-10 left-10 border-[0.1px] border-[#454141] bg-[#FF0000]"></div>
+            ) : (
+              <div className="w-3 h-5 rounded absolute sm:top-12 sm:right-2 top-10 right-10 border-[0.1px] border-[#454141] bg-[#FF0000]"></div>
+            )}
           </>
-          
+
         )}
         {userRank === 1 ? (
           <>
-          {width > 650 ? (
-            <div className="sm:w-12 w-10 sm:h-12 h-10 rounded absolute top-8 sm:left-11 left-7 z-10">
-              <img className="z-10" src="/images/1st.png" alt="1st"></img>
-          </div>
-          ):(
-            <div className="sm:w-12 w-10 sm:h-12 h-10 rounded absolute top-8 sm:right-10 right-12 z-10">
-              <img className="z-10" src="/images/1st.png" alt="1st"></img>
-          </div>
-          )}
+            {width > 650 ? (
+              <div className="sm:w-12 w-10 sm:h-12 h-10 rounded absolute top-8 sm:left-11 left-7 z-10">
+                <img className="z-10" src="/images/1st.png" alt="1st"></img>
+              </div>
+            ) : (
+              <div className="sm:w-12 w-10 sm:h-12 h-10 rounded absolute top-8 sm:right-10 right-12 z-10">
+                <img className="z-10" src="/images/1st.png" alt="1st"></img>
+              </div>
+            )}
           </>
-          
+
         ) : userRank === 2 ? (
           <>
-          {width > 650 ? (
-            <div className="sm:w-12 w-10 sm:h-12 h-10 rounded absolute top-8 sm:left-11 left-7 z-10">
-            <img className="z-10" src="/images/2nd.png" alt="2nd"></img>
-          </div>
-          ):(
-            <div className="sm:w-12 w-10 sm:h-12 h-10 rounded absolute top-8 sm:right-10 right-12 z-10">
-            <img className="z-10" src="/images/2nd.png" alt="2nd"></img>
-          </div>
-          )}
-          
+            {width > 650 ? (
+              <div className="sm:w-12 w-10 sm:h-12 h-10 rounded absolute top-8 sm:left-11 left-7 z-10">
+                <img className="z-10" src="/images/2nd.png" alt="2nd"></img>
+              </div>
+            ) : (
+              <div className="sm:w-12 w-10 sm:h-12 h-10 rounded absolute top-8 sm:right-10 right-12 z-10">
+                <img className="z-10" src="/images/2nd.png" alt="2nd"></img>
+              </div>
+            )}
+
           </>
         ) : userRank === 3 ? (
           <>
-          {width > 650 ? (
-            <div className="sm:w-12 w-10 sm:h-12 h-10 rounded absolute top-8 sm:left-11 left-7 z-10">
-            <img className="z-10" src="/images/3rd.png" alt="3rd"></img>
-          </div>
-          ) :(
-            <div className="sm:w-12 w-10 sm:h-12 h-10 rounded absolute top-8 sm:right-10 right-12 z-10">
-            <img className="z-10" src="/images/3rd.png" alt="3rd"></img>
-          </div>
-          )}
-          
+            {width > 650 ? (
+              <div className="sm:w-12 w-10 sm:h-12 h-10 rounded absolute top-8 sm:left-11 left-7 z-10">
+                <img className="z-10" src="/images/3rd.png" alt="3rd"></img>
+              </div>
+            ) : (
+              <div className="sm:w-12 w-10 sm:h-12 h-10 rounded absolute top-8 sm:right-10 right-12 z-10">
+                <img className="z-10" src="/images/3rd.png" alt="3rd"></img>
+              </div>
+            )}
+
           </>
         ) : (
           <div>
