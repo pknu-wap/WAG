f380f69ee059096831212e6c208698a2688567db
JongKyeong Kim <oo7bangjk@naver.com>
2025-06-10 10:51:17 +0900

    feat: 일정 시간 이상 게임을 시작하지 않은 방은 삭제 처리

--- server/src/main/java/com/example/server/ServerApplication.java
+++ server/src/main/java/com/example/server/ServerApplication.java
@@ -4,11 +4,12 @@ import com.example.server.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 @EnableConfigurationProperties(AppProperties.class)
 public class ServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 	}
