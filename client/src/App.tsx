ffa938b3520d23adda683ca84ca274e99ec508ee
Junseo Kim <semiwest2@gmail.com>
2025-11-22 12:09:30 +0900

    refactor: 404 페이지 대신 잘못된 경로 접근 시 메인페이지로 리다이렉트
    사용자가 존재하지 않는 경로로 접근할 때 404 페이지 대신 메인페이지로 이동하도록 변경하여 UX 개선
    - App.tsx에서 <Route path='/*'>를 <Navigate to='/' />로 변경
    - Navigate import 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,10 @@
 import { useEffect } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import MainPage from './pages/Main';
 import CreateRoom from './pages/CreateRoom';
 import GameRoom from './pages/GameRoom';
-import WrongUrl from './components/layout/404';
 import { initializeGA } from './util/googleAnalytics/initializeGA';
-function isChrome() {
-  const userAgent = window.navigator.userAgent;
-  return userAgent.includes('Chrome') || userAgent.includes('Safari');
-}
 
 function App() {
   useEffect(() => {
@@ -22,7 +17,7 @@ function App() {
         <Route path='/' element={<MainPage />} />
         <Route path='/CreateRoom' element={<CreateRoom />} />
         <Route path='/GameRoom/:roomId' element={<GameRoom />} />
-        <Route path='/*' element={<WrongUrl />} />
+        <Route path='/*' element={<Navigate to='/' />} />
       </Routes>
     </AnimatePresence>
   );
