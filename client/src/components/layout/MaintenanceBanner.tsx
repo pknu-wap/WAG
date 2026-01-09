3ebfe24865247c84d2c7ffc0cdd4c13ce37e5f0a
Jang Hong Joon <35947667+wkdghdwns199@users.noreply.github.com>
2026-01-09 22:03:18 +0900

    Update maintenance message date to January 11, 2026

--- client/src/components/layout/MaintenanceBanner.tsx
+++ client/src/components/layout/MaintenanceBanner.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const MaintenanceBanner: React.FC = () => {
-  const message = '1월 10일 0시에 서버가 일시 중단됩니다! 플레이에 참고 바랍니다! 1월 14일에 다시 돌아올게요!';
+  const message = '2026년 1월 11일 0시에 서버가 일시 중단됩니다! 플레이에 참고 바랍니다! 1월 14일에 다시 돌아올게요!';
 
   const renderMessages = () => {
     return Array.from({ length: 6 }).map((_, index) => (
